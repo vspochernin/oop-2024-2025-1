592ec7b2def8bf1bddf6b2f2bd0a6d243a94a6a9
Vladislav Pochernin <vspochernin@gmail.com>
2024-10-12 14:34:35 +0300

    initial commit for lab-2.8

---
+++ lab-2.8/src/main/java/ru/vspochernin/Main.java
@@ -0,0 +1,10 @@
+package ru.vspochernin;
+
+/**
+ * @author pochernin-vla
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
