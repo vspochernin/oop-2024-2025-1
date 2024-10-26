a7286a9d743ffeb52f8a319852171a5fbe31ccc1
Vladislav Pochernin <vspochernin@gmail.com>
2024-10-26 22:07:40 +0300

    lab-2.8: complete pawn tests

--- lab-2.8/src/test/java/ru/vspochernin/pieces/PawnTest.java
+++ lab-2.8/src/test/java/ru/vspochernin/pieces/PawnTest.java
@@ -10,45 +10,83 @@ import ru.vspochernin.utils.TestUtils;
 class PawnTest extends TestWithBoard {
 
     @Test
-    public void testWhitePawnInitialShortMove() {
-        TestUtils.acceptMovesToTheBoardWithoutExceptions(board, List.of("a2 a3"));
-    }
-
-    @Test
-    public void testWhitePawnInitialLongMove() {
-        TestUtils.acceptMovesToTheBoardWithoutExceptions(board, List.of("a2 a4"));
-    }
-
-    @Test
-    public void testWhitePawnInitialIllegalMoves() {
-        TestUtils.acceptMovesToTheBoardWithIllegalMoveReasonAtTheEnd(
+    public void successfulPawnOneCellMove() {
+        TestUtils.acceptMovesToTheBoardWithoutExceptions(
                 board,
-                IllegalMoveReason.FROM_EQUALS_TO,
-                List.of("a2 a2"));
+                List.of(
+                        "e2 e3",
+                        "d7 d6"));
+    }
 
+    @Test
+    public void successfulPawnTwoCellMove() {
+        TestUtils.acceptMovesToTheBoardWithoutExceptions(
+                board,
+                List.of(
+                        "e2 e4",
+                        "d7 d5"));
+    }
 
+    @Test
+    public void blockedPawnOneCellMoveFail() {
+        TestUtils.acceptMovesToTheBoardWithoutExceptions(
+                board,
+                List.of(
+                        "e2 e4",
+                        "d7 d5",
+                        "e4 e5",
+                        "d5 d4",
+                        "e5 e6",
+                        "d4 d3"));
         TestUtils.acceptMovesToTheBoardWithIllegalMoveReasonAtTheEnd(
                 board,
                 IllegalMoveReason.PAWN_ILLEGAL_MOVE,
-                List.of("a2 a5"));
+                List.of("d2 d3"));
+        TestUtils.acceptMovesToTheBoardWithIllegalMoveReasonAtTheEnd(
+                board,
+                IllegalMoveReason.PAWN_ILLEGAL_MOVE,
+                List.of("a2 a3",
+                        "e7 e6"));
     }
 
     @Test
-    public void testWhitePawnAttacks() {
+    public void blockedPawnTwoCellMoveFail() {
         TestUtils.acceptMovesToTheBoardWithoutExceptions(
                 board,
                 List.of(
-                        "a2 a4",
-                        "b7 b5",
-                        "a4 b5"));
+                        "e2 e4",
+                        "d7 d5",
+                        "e4 e5",
+                        "d5 d4",
+                        "e5 e6",
+                        "d4 d3"));
+        TestUtils.acceptMovesToTheBoardWithIllegalMoveReasonAtTheEnd(
+                board,
+                IllegalMoveReason.PAWN_ILLEGAL_MOVE,
+                List.of("d2 d4"));
+        TestUtils.acceptMovesToTheBoardWithIllegalMoveReasonAtTheEnd(
+                board,
+                IllegalMoveReason.PAWN_ILLEGAL_MOVE,
+                List.of("a2 a3",
+                        "e7 e5"));
     }
 
     @Test
-    public void testBlackPawnInitialShortMove() {
+    public void successfulPawnEats() {
         TestUtils.acceptMovesToTheBoardWithoutExceptions(
                 board,
                 List.of(
-                        "a2 a4",
-                        "a7 a6"));
+                        "c2 c4",
+                        "c7 c5",
+                        "d2 d4",
+                        "d7 d5",
+                        "e2 e4",
+                        "e7 e5",
+                        "f2 f4",
+                        "f7 f5",
+                        "c4 d5",
+                        "c5 d4",
+                        "f4 e5",
+                        "f5 e4"));
     }
 }
