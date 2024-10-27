b5b3678948366260d935b7a165b9381813d632b5
Vladislav Pochernin <vspochernin@gmail.com>
2024-10-27 12:16:11 +0300

    lab-2.8: final bishop tests

---
+++ lab-2.8/src/test/java/ru/vspochernin/pieces/BishopTest.java
@@ -0,0 +1,64 @@
+package ru.vspochernin.pieces;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import ru.vspochernin.TestWithBoard;
+import ru.vspochernin.exception.IllegalMoveReason;
+import ru.vspochernin.utils.TestUtils;
+
+/**
+ * @author pochernin-vla
+ */
+class BishopTest extends TestWithBoard {
+
+    @Test
+    public void successfulBishopMove() {
+        TestUtils.acceptMovesToTheBoardWithoutExceptions(
+                board,
+                List.of(
+                        "b2 b3",
+                        "b7 b6",
+                        "c1 a3",
+                        "c8 a6",
+                        "a3 d6",
+                        "a6 d3",
+                        "d6 f4",
+                        "d3 f5"));
+    }
+
+    @Test
+    public void successfulBishopEats() {
+        TestUtils.acceptMovesToTheBoardWithoutExceptions(
+                board,
+                List.of(
+                        "b2 b3",
+                        "b7 b6",
+                        "c1 a3",
+                        "c8 a6",
+                        "a3 e7",
+                        "a6 e2"));
+    }
+
+    @Test
+    public void bishopCrazyMoveFail() {
+        TestUtils.acceptMovesToTheBoardWithIllegalMoveReasonAtTheEnd(
+                board,
+                IllegalMoveReason.BISHOP_ILLEGAL_MOVE,
+                List.of(
+                        "b2 b3",
+                        "b7 b6",
+                        "c1 a3",
+                        "c8 a6",
+                        "a3 g5"));
+    }
+
+    @Test
+    public void bishopChessPiecesBetweenMoveFail() {
+        TestUtils.acceptMovesToTheBoardWithIllegalMoveReasonAtTheEnd(
+                board,
+                IllegalMoveReason.CHESS_PIECES_BETWEEN,
+                List.of(
+                        "c1 f4"));
+    }
+}
